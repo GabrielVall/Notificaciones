4a804caa930ee6cc3fad74d0f11b98b464d50a8a
GabrielV <gabrielvallejo2000@gmail.com>
2022-06-28 11:54:08 -0500

    Prueba servidor

--- notif.php
+++ notif.php
@@ -1,13 +1,18 @@
 <?php
+$app_id = "63c06cdb-08b6-4806-b15d-f97fc454d699";
+$app_secret = "OGU3ZmRlNzUtZTBiZi00M2I3LThjNDYtOTc5YjMxZGJlNTU5";
 require_once('vendor/autoload.php');
 
 $client = new \GuzzleHttp\Client();
 
 $response = $client->request('POST', 'https://onesignal.com/api/v1/notifications', [
-  'body' => '{"included_segments":["Subscribed Users"],"contents":{"en":"English or Any Language Message","es":"Spanish Message"},"name":"INTERNAL_CAMPAIGN_NAME"}',
+  'body' => '{"app_id":"'.$app_id.'",
+    "included_segments":["Subscribed Users"],
+    "contents":{"en":"English or Any Language Message","es":"Spanish Message"},
+    "name":"INTERNAL_CAMPAIGN_NAME"}',
   'headers' => [
     'Accept' => 'application/json',
-    'Authorization' => 'Basic NzZlYWNlNmUtY2U5Ny00MmU5LTg5ZWEtZmFkOGNjNTdkZWNh',
+    'Authorization' => 'Basic '.$app_secret,
     'Content-Type' => 'application/json',
   ],
 ]);
